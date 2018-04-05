bf8674b3d0073e572200b44e2d6735238c151a0e
admin <admin@iMac3.local>
2018-04-05 16:43:16 +0200

    Add some fonction to the little game at te top of the page

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 //Select the div via it's id and store the selected element inside gameContainer var.
 let gameContainer = document.querySelector('#game-container');
+let resetButton = document.querySelector('#reset-button');
+let score = 0;
+const casNumber = 6;
+
+function init(){
+  let gameContainerDiv = document.createElement('div');
+  gameContainerDiv.id = "game-container";
+  for (var i = 1; i <= casNumber; i++) {
+    let tempDiv = document.createElement('div');
+    tempDiv.className = "green-color";
+    gameContainerDiv.appendChild(tempDiv);
+  }
+  document.querySelector(' data-use=gameBoardContainer').appendChild(gameContainerDiv);
+  let reinitButton = document.createElement('input');
+  reinitButton.setAttribute('type', 'button');
+  reinitButton.setAttribute('value', 'reinit');
+  reinitButton.setAttribute('data-action', 'reinit');
+}
+
 function invertColor(div){
   let green = "green-color";
   let red = "red-color";
@@ -11,7 +30,34 @@ function invertColor(div){
     div.classList.add(green);
   };
 };
+function restart(div){
+  let green = "green-color";
+  let red = "red-color"
+  if(div.classList.contains(red)){
+    div.classList.remove(red);
+    div.classList.add(green);
+  };
+};
+//Defeat condition
+function checkDefeat(divs){
+  let defeatStatus = true ;
+  for (let i = 0; i < divs.length; i++) {
+    if(divs[i].classList.contains('green-color')){
+      defeatStatus = true;
+      break;
+    }
+  }
+    if(defeatStatus == true) {
+      setTimeout(function(){
+        alert('you lose');
+      }, 500);
+    }
+  }
 
+
+
+
+//Victory check
 function checkVictory(divs){
   let victoryStatus = true;
   for (let i = 0; i < divs.length; i++) {
@@ -24,20 +70,26 @@ function checkVictory(divs){
   if(victoryStatus == true) {
     setTimeout(function(){
     alert('GG WP!');
+
   }, 500);
+
   }
-  //si toutes divs sont rouge GG
-  //si aucune divs n'est verte GG
+
 };
+  document.getElementById('compteur').innerHTML = score;
 
 //When someone clicks on anything inside the game container, it triggers the anonymous function.
 gameContainer.addEventListener('click', function(el){
+
+
 
   let childDivs = document.querySelectorAll('#game-container>div');
 //We get the target of te click event, which is the specific div and not the container div.
   let clickedElement = el.target;
 
+
   for(let i = 0; i < childDivs.length; i++)
+
 
   {
 //childDivs[i] allow us ti display every value of the childDivs Array because we select it via the array key [i]
@@ -46,15 +98,19 @@ gameContainer.addEventListener('click', function(el){
     //if this is the div we clicked on.
      if (childDivs[i] == clickedElement) {
 
+
        if (i > 0 && i<(childDivs.length - 1)) {
          invertColor(childDivs [i + 1]);
          invertColor(childDivs [i - 1]);
+         invertColor(childDivs [i]);
           // on va inverser la couleur de la div gauche et de la div droite
        }  else if (i == 0) {
          invertColor(childDivs [i + 1]);
+         invertColor(childDivs [i]);
          //on va inverser la couleur de la div  droite
        }  else if ( i == (childDivs.length - 1)) {
          invertColor(childDivs [i - 1]);
+         invertColor(childDivs [i]);
          //on va inverser la couleur de la div gauche
        }
 
@@ -66,5 +122,30 @@ gameContainer.addEventListener('click', function(el){
 
     }
   }
+    score++;
+    document.getElementById('compteur').innerHTML = score;
+    if(score > 4){
+      checkDefeat(childDivs);
+
+    }
+
     checkVictory(childDivs);
+
 });
+
+  resetButton.addEventListener('click',function(el){
+    let childDivs = document.querySelectorAll('#game-container>div');
+    let reset = document.querySelectorAll('#reset-button>div');
+    for(let i = 0; i < childDivs.length; i ++){
+      restart(childDivs [i]);
+      score = 0;
+      document.getElementById('compteur').innerHTML = score;
+
+    }
+
+
+
+
+
+
+  });
